8717c398c218840d61f3471f6492669dc1e5ca75
Keita <66842133+keitaabdou@users.noreply.github.com>
2021-06-08 22:32:49 +0000

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+         return view('tasks.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|max:100',
+            'detail' => 'required|max:500'
+        ]);
+
+        $task = new Task;
+        $task->title = $request->title;
+        $task->detail = $request->detail;
+        $task->save();
+
+        return back()->with('message', 'La tache a bien été crée');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        return view('tasks.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        return view('tasks.edit', compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+        {
+            $data = $request->validate([
+                'title' => 'required|max:100',
+                'detail' => 'required|max:500',
+            ]);
+
+            $task->title = $request->title;
+            $task->detail = $request->detail;
+            $task->state = $request->has('state');
+            $task->save();
+
+            return back()->with('message', "La tâche a bien été modifiée !");
+        }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+    return back();
+    }
+}
